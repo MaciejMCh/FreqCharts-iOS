71f2aeb994652ac0b7610a29908ff08c126a2ba7
Maciej Chmielewski <maciej.chmielewski@appunite.com>
2015-10-13 15:48:46 +0200

    bubble internal constraints

--- FreqCharts/FCBubbleCollectionViewCell.swift
+++ FreqCharts/FCBubbleCollectionViewCell.swift
@@ -11,6 +11,9 @@ import UIKit
 class FCBubbleCollectionViewCell: UICollectionViewCell {
     
     var webView = UIWebView ()
+    var widthConstraint: NSLayoutConstraint!
+    var heightConstraint: NSLayoutConstraint!
+    
     
     override init(frame: CGRect) {
         super.init(frame: frame)
@@ -18,9 +21,21 @@ class FCBubbleCollectionViewCell: UICollectionViewCell {
         self.clipsToBounds = true
         
         self.webView.backgroundColor = UIColor.redColor()
-        
-        
         self.contentView.addSubview(self.webView)
+        
+        self.initConstraints()
+    }
+    
+    func initConstraints() {
+        self.webView.translatesAutoresizingMaskIntoConstraints = false
+        
+        self.widthConstraint = NSLayoutConstraint(item: self.webView, attribute: .Height, relatedBy: .Equal, toItem: nil, attribute: .NotAnAttribute, multiplier: 1.0, constant: 100.0)
+        self.heightConstraint = NSLayoutConstraint(item: self.webView, attribute: .Width, relatedBy: .Equal, toItem: nil, attribute: .NotAnAttribute, multiplier: 1.0, constant: 100.0)
+        
+        let xCenterConstraint = NSLayoutConstraint(item: self.contentView, attribute: .CenterX, relatedBy: .Equal, toItem: self.webView, attribute: .CenterX, multiplier: 1, constant: 0)
+        let yCenterConstraint = NSLayoutConstraint(item: self.contentView, attribute: .CenterY, relatedBy: .Equal, toItem: self.webView, attribute: .CenterY, multiplier: 1, constant: 0)
+        
+        self.contentView.addConstraints([self.widthConstraint, self.heightConstraint, xCenterConstraint, yCenterConstraint])
     }
     
     required init?(coder aDecoder: NSCoder) {
